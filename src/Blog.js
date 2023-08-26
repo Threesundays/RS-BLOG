861009230e348fddb51066bd3d4fabc2e575aa34
Anton Krat <threesundaysonewerk@gmail.com>
2023-08-26 20:29:34 +0800

    Реализовать шапку

--- src/Blog.js
+++ src/Blog.js
@@ -1,21 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
+import { Header } from './components'
 import styled from 'styled-components';
+
+const AppColumn = styled.div`
+display: flex
+flex-direction: column;
+justify-content: space-between;
+width: 1000px;
+min-height: 100%;
+margin: 0 auto;
+background-color: #fff;
+`;
 
 const Content = styled.div`
 	padding: 120px 0;
 `;
 
 const H2 = styled.h2`
-text-align: center;
+	text-align: center;
 `;
-
-const Header = () => {
-	return (
-		<div>
-			<h1>Header</h1>
-		</div>
-	);
-};
 
 const Footer = () => {
 	return (
@@ -27,7 +30,7 @@ const Footer = () => {
 
 export const Blog = () => {
 	return (
-		<>
+		<AppColumn>
 			<Header />
 			<Content>
 				<H2>Контент страницы</H2>
@@ -42,6 +45,6 @@ export const Blog = () => {
 				</Routes>
 			</Content>
 			<Footer />
-		</>
+		</AppColumn>
 	);
 };
