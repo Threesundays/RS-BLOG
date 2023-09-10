e52c620b6fe56dd4a50e167dcc700a5dc09b2fc0
Anton Krat <threesundaysonewerk@gmail.com>
2023-09-10 21:00:14 +0800

    Реализовать страницу Статья

--- src/Blog.js
+++ src/Blog.js
@@ -1,6 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import { Header, Footer } from './components';
-import { Authorization, Registration, Users } from './pages';
+import { Authorization, Post, Registration, Users } from './pages';
+import { useDispatch } from 'react-redux';
+import { useLayoutEffect } from 'react';
+import { setUser } from './actions';
 import styled from 'styled-components';
 
 const AppColumn = styled.div`
@@ -14,10 +17,27 @@ const AppColumn = styled.div`
 `;
 
 const Page = styled.div`
-	padding: 120px 0;
+	padding: 120px 0 20px;
 `;
 
 export const Blog = () => {
+	const dispatch = useDispatch()
+
+	useLayoutEffect(() => {
+		const currentUserDataJSON = sessionStorage.getItem('userData');
+
+		if (!currentUserDataJSON) {
+			return;
+		}
+
+		const currentUserData = JSON.parse(currentUserDataJSON);
+
+		dispatch(setUser({
+			...currentUserData,
+			roleId: Number(currentUserData.roleId),
+		}));
+	}, [dispatch]);
+
 	return (
 		<AppColumn>
 			<Header />
@@ -28,7 +48,7 @@ export const Blog = () => {
 					<Route path="/register" element={<Registration />} />
 					<Route path="/users" element={<Users />} />
 					<Route path="/post" element={<div>Новая статья</div>} />
-					<Route path="/post/:postId" element={<div>Статья</div>} />
+					<Route path="/post/:id" element={<Post />} />
 					<Route path="*" element={<div>Ошибка</div>} />
 				</Routes>
 			</Page>
